b06f40b6d848cc5b2b170d549af5816fbd6442fc
KAWU16 <73186439+KAWU16@users.noreply.github.com>
2021-10-28 21:17:03 +0200

    asd

--- static/my_js/cookie.js
+++ static/my_js/cookie.js
@@ -1,13 +1,41 @@
-const cookieContainer = document.querySelector(".cookie-container");
-const cookieButton = document.querySelector(".cookie-btn");
+function getCookie(cname) {
+  const name = cname + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(';');
 
-cookieButton.addEventListener("click", () => {
-  cookieContainer.classList.remove("active");
-  localStorage.setItem("cookieBannerDisplayed", "true");
-});
-
-setTimeout(() => {
-  if (!localStorage.getItem("cookieBannerDisplayed")) {
-    cookieContainer.classList.add("active");
+  for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf(name) === 0) {
+          return c.substring(name.length, c.length);
+      }
   }
-}, 2000);
+
+  return "";
+}
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+
+  const expires = "expires="+ d.toUTCString();
+
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+const cookieBanner = document.querySelector('#cookie-banner');
+const hasCookieConsent = getCookie('cookies-consent');
+
+if (!hasCookieConsent) {
+    cookieBanner.classList.remove('hidden');
+}
+
+const consentCta = cookieBanner.querySelector('#consent-cookies');
+
+consentCta.addEventListener('click', () => {
+    cookieBanner.classList.add('hidden');
+    setCookie('cookies-consent', 1, 365);
+});
