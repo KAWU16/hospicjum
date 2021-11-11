2d04beeec92599860f12fea212df2f6c2da8d982
KAWU16 <73186439+KAWU16@users.noreply.github.com>
2021-11-11 13:25:41 +0100

    Update script.js

--- themes/vixcon-hugo/assets/js/script.js
+++ themes/vixcon-hugo/assets/js/script.js
@@ -20,8 +20,6 @@
   });
 
 })(jQuery);
-import LocomotiveScroll from "https://cdn.skypack.dev/locomotive-scroll";
-
 const scroller = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),
   smooth: true
